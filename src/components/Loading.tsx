0a124bb10a037768c0d51dadf0a1942759214c06
Shani Vaishnav <157327364+sunnyvaishnav7@users.noreply.github.com>
2026-03-15 14:57:49 +0530

    Refactor Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import "./styles/Loading.css";
 import { useLoading } from "../context/LoadingProvider";
 
-import Marquee from "react-fast-marquee";
 
 const Loading = ({ percent }: { percent: number }) => {
   const { setIsLoading } = useLoading();
