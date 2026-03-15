288152f6929fad51e8fe7d682563133bc4beeb88
Shani Vaishnav <157327364+sunnyvaishnav7@users.noreply.github.com>
2026-03-15 15:40:29 +0530

    Refactor Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
@@ -50,7 +50,7 @@ const Navbar = () => {
           className="navbar-connect"
           data-cursor="disable"
         >
-         Shanivaishnav7@gmail.com
+          Shanivaishnav7@gmail.com
         </a>
         <ul>
           <li>
